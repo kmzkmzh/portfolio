d8e147b116a3dc35d28f394413ccce496717098d
kmzkmzh <167289873+kmzkmzh@users.noreply.github.com>
2024-07-10 13:44:11 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// JavaScript
+//console.log('Hello world!');
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  /*direction: 'vertical',*/
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
